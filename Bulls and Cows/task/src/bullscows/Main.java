aea26a0d51842b8c77d7b1c3b7a522df109678f9
AluvarD <antipov_ilya26@mail.ru>
2020-11-30 20:10:39 +0300

    stage 3 complate

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,4 +1,5 @@
 package bullscows;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,15 +7,60 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int answer;
         int secretNumber = 1234;
+        int count;
+        while (true) {
+            count = scanner.nextInt();
+            if (count > 10) {
+                System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            } else {
+                break;
+            }
+        }
+        long generatedSecretNumber = generateSecretNumber(count);
+        System.out.println("The random secret number is " + generatedSecretNumber);
         int turn = 1;
-        do {
+        /*do {
             System.out.println("Turn " + turn + ". Answer:");
             answer = scanner.nextInt();
             turn++;
-        } while (checkAnswer(secretNumber, answer));
+        } while (checkAnswerForInt(secretNumber, answer));*/
     }
 
-    public static boolean checkAnswer(int secretNumber, int answer) {
+    public static long generateSecretNumber(int count){
+
+        StringBuilder secretNumber = new StringBuilder();
+        long number;
+        while (secretNumber.length() < count) {
+            StringBuilder randomGenerated = new StringBuilder(String.valueOf(System.nanoTime()));
+            randomGenerated.reverse();
+            for (int i = 0; i < randomGenerated.length(); i++) {
+                if (secretNumber.length() == 0) {
+                    if (randomGenerated.charAt(i) != '0') {
+                        secretNumber.append(randomGenerated.charAt(i));
+                    }
+                } else {
+                    boolean isInclude = false;
+                    for (int j = 0; j < secretNumber.length(); j++){
+                        if (secretNumber.charAt(j) == randomGenerated.charAt(i)) {
+                            isInclude = true;
+                            break;
+                        }
+                    }
+                    if (!isInclude) {
+                        secretNumber.append(randomGenerated.charAt(i));
+                    }
+                }
+                if (secretNumber.length() == count){
+                    break;
+                }
+            }
+        }
+        String convert = secretNumber.toString();
+        number = Long.parseLong(convert);
+        return number;
+    }
+
+    public static boolean checkAnswerForInt(int secretNumber, int answer) {
         if (secretNumber == answer) {
             System.out.println("Grade: 4 bulls.");
             System.out.println("Congrats! The secret code is " + secretNumber + ".");
