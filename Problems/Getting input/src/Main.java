98bf52cdc55d01669a51ae06b2cf7b4090eb2a9e
AluvarD <antipov_ilya26@mail.ru>
2020-12-03 21:21:28 +0300

    stage 6 package

---
+++ Problems/Getting input/src/Main.java
@@ -0,0 +1,8 @@
+class InputClass {
+    public static String getString() {
+        // write your code here
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
+}
